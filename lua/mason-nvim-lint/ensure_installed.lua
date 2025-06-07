2442e830879daf400bf148667f31ba6e8188ab99
Sujal <email@sujal.tv>
2025-06-07 15:07:23 -0400

    fix: installing linters with error check

--- lua/mason-nvim-lint/ensure_installed.lua
+++ lua/mason-nvim-lint/ensure_installed.lua
@@ -5,7 +5,15 @@ local settings = require "mason-nvim-lint.settings"
 local function ensure_installed()
     for _, linter_name in ipairs(settings.current.ensure_installed) do
         local mason_linter_identifier = mapping.nvimlint_to_package[linter_name]
-        require("mason-nvim-lint.install").try_install(mason_linter_identifier)
+
+        if mason_linter_identifier then
+            require("mason-nvim-lint.install").try_install(mason_linter_identifier)
+        elseif not settings.current.quiet_mode then
+            vim.notify(
+                "mason-nvim-lint: linter " .. linter_name .. " not installed.",
+                vim.log.levels.WARN
+            )
+        end
     end
 end
 
